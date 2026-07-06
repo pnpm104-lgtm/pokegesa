9a6a6e80f157915a88d4fcf62a072ec779cab881
pnpm104-lgtm <pnpm104@gmail.com>
2026-07-06 14:32:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>Poke Guesser</title>
-    <!-- 開いた瞬間に game.html に自動で切り替える設定 -->
-    <meta http-equiv="refresh" content="0;URL=game.html">
-</head>
-<body>
-    <p>ゲーム画面に移動しています...</p>
-</body>
-</html>
+
